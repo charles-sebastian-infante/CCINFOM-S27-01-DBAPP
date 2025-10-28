3cac747f811b33758236f1241dee23e3712baf93
Floranaras <carlosranara0@gmail.com>
2025-10-28 16:35:26 +0800

    feat: full mvc skeleton & route and terminal completed

---
+++ src/main/java/com/busterminal/model/Route.java
@@ -0,0 +1,122 @@
+package com.busterminal.model;
+
+import java.sql.*;
+import com.busterminal.utils.DBConnection;
+
+public class Route {
+    public int routeId;
+    public String routeName;
+    public int originId;
+    int destinationID;
+    public double distance;
+    public String travelTime;
+    public double baseFare;
+
+    public Route() {
+        routeId = 0;
+        routeName = "";
+        originId = 0;
+        destinationID = 0;
+        distance = 0.0;
+        travelTime = "";
+        baseFare = 0.0;
+    }
+
+    public int addRecord() {
+        try {
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement pStmt = conn.prepareStatement(
+                "INSERT INTO Route (route_name, origin_id, destination_id," +
+                " distance, travel_time, base_fare) VALUES (?,?,?,?,?,?)");
+            pStmt.setString(1, routeName);
+            pStmt.setInt(2, originId);
+            pStmt.setInt(3, destinationID);
+            pStmt.setDouble(4, distance);
+            pStmt.setString(5, travelTime);
+            pStmt.setDouble(6, baseFare);
+            pStmt.executeUpdate();
+            pStmt.close();
+            conn.close();
+            return 1;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+
+    public int modRecord() {
+        try {
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement pStmt = conn.prepareStatement(
+                "UPDATE Route SET route_name = ?, origin_id = ?," +
+                " destination_id = ?," + " distance = ?, travel_time = ?," +
+                " base_fare = ?, WHERE route_id = ?");
+            pStmt.setString(1, routeName);
+            pStmt.setInt(2, originId);
+            pStmt.setInt(3, destinationID);
+            pStmt.setDouble(4, distance);
+            pStmt.setString(5, travelTime);
+            pStmt.setDouble(6, baseFare);
+            pStmt.setInt(7, routeId);
+            pStmt.executeUpdate();
+            pStmt.close();
+            conn.close();
+            return 1; 
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+
+    public int delRecord() {
+        try {
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement pStmt = conn.prepareStatement(
+                "DELETE FROM Route WHERE route_id = ?");
+            pStmt.setInt(1, routeId);
+            pStmt.executeUpdate();
+            pStmt.close();
+            conn.close();
+            return 1;  
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+
+    }
+
+    public int getRecord() {
+        try {
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement pStmt = conn.prepareStatement(
+                "SELECT * FROM Route WHERE route_id = ?");
+            pStmt.setInt(1, routeId);
+            ResultSet rs = pStmt.executeQuery();
+            
+            routeId = 0;
+            routeName = "";
+            originId = 0;
+            destinationID = 0;
+            distance = 0.0;
+            travelTime = "";
+            baseFare = 0.0;
+
+            while (rs.next()) {
+                routeId = rs.getInt("route_id");
+                routeName = rs.getString("route_name");
+                originId = rs.getInt("origin_id");
+                destinationID = rs.getInt("destination_id");
+                distance = rs.getDouble("distance");
+                travelTime = rs.getString("travel_time");
+                baseFare = rs.getDouble("base_fare");
+            }
+            rs.close();
+            pStmt.close();
+            conn.close();
+            return 1;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+}
---
+++ src/main/java/com/busterminal/model/Terminal.java
@@ -0,0 +1,113 @@
+package com.busterminal.model;
+
+import java.sql.*;
+import com.busterminal.utils.DBConnection;
+
+public class Terminal {
+    public int terminalID;
+    public String terminalName;
+    public String location;
+    public String city;
+    public String phone;
+
+    public Terminal() {
+        terminalID = 0;
+        terminalName = "";
+        location = "";
+        city = "";
+        phone = "";
+    }
+
+    public int addRecord() {
+        try {
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement pStmt = conn.prepareStatement(
+                "INSERT INTO Terminal (terminal_name, location, city, phone" +
+                " VALUES (?,?,?,?)");
+            pStmt.setString(1, terminalName);
+            pStmt.setString(2, location);
+            pStmt.setString(3, city);
+            pStmt.setString(4, phone);
+            pStmt.executeUpdate();
+            pStmt.close();
+            conn.close();
+            return 1;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+
+    public int modRecord() {
+        try {
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement pStmt = conn.prepareStatement(
+                "UPDATE Terminal SET terminal_name = ?,  location = ?, " + 
+                "city = ?, phone = ? WHERE terminal_id = ?");
+            pStmt.setString(1, terminalName);
+            pStmt.setString(2, location);
+            pStmt.setString(3, city);
+            pStmt.setString(4, phone);
+            pStmt.setInt(5, terminalID);
+            pStmt.executeUpdate();
+            pStmt.close();
+            conn.close();
+            return 1;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+
+    public int delRecord() {
+        try {
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement pStmt = conn.prepareStatement(
+                "DELETE FROM Terminal WHERE terminal_id = ?");
+            pStmt.setInt(1, terminalID);
+            pStmt.executeUpdate();
+            pStmt.close();
+            conn.close();
+            return 1;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+
+    public int getRecord() {
+        try {
+            Connection conn = DBConnection.getConnection();
+            PreparedStatement pStmt = conn.prepareStatement(
+                "SELECT * FROM Terminal WHERE terminal_id = ?");
+            pStmt.setInt(1, terminalID);
+            ResultSet rs = pStmt.executeQuery();
+
+            terminalID = 0;
+            terminalName = "";
+            location = "";
+            city = "";
+            phone = "";
+
+            while (rs.next()) {
+                terminalID = rs.getInt("terminal_id");
+                terminalName = rs.getString("terminal_name");
+                location = rs.getString("location");
+                city = rs.getString("city");
+                phone = rs.getString("phone");
+            }
+
+            rs.close();
+            pStmt.close();
+            conn.close();
+            return 1;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    } 
+}
+
+
+
+
